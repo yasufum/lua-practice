3d8d642dcb9e81bec5bb0569e1fec22d08e1827e
Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>
2018-04-24 13:16:06 +0900

    Add hello example.
    Signed-off-by: Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>

---
+++ hello/kogure.lua
@@ -0,0 +1,34 @@
+#!/usr/bin/env lua
+
+--[[
+-- Calculate daemonized age from birth year, month and date.
+--]]
+function age_daemon(by, bm, bd)
+  today = os.date("*t")
+  y = today["year"] - by
+  if (today["month"] < bm) then
+    y = y - 1
+  elseif (today["month"] == bm) then
+    print(today["day"])
+    print(bd)
+    if (today["day"] < bd) then
+      y = y - 1
+      print(y)
+    end
+  end
+
+  -- daemonize!
+  y = y + 100000
+  return y
+end
+
+function main()
+  -- He was born on Nov. 10th, 98038 AC actually, but born in 1962 in
+  -- the human world!
+  k_name = "Daemon Kogure"
+  k_age = age_daemon(1962, 11, 10)
+
+  print(k_name .. " is " .. string.format("%d", k_age) .. " years old!")
+end
+
+main()
