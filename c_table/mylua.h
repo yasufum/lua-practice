287ac79d47f431baf7a3267a5bb554f00ee4599c
Yasufumi Ogawa <yasufum.o@gmail.com>
2019-10-11 13:30:47 +0900

    Update lua to 5.3 in c_table
    Signed-off-by: Yasufumi Ogawa <yasufum.o@gmail.com>

--- c_table/mylua.h
+++ c_table/mylua.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-#include "lua.h"
-#include "lualib.h"
-#include "lauxlib.h"
+#include <lua5.3/lua.h>
+#include <lua5.3/lualib.h>
+#include <lua5.3/lauxlib.h>
 
 void dumpStack(lua_State* L);
